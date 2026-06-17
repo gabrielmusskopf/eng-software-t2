5e0bfe529d16d056e67548b0e612bf79dba2bff1
Gabriel Musskopf <gabrielgmusskopf@gmail.com>
2026-06-16 21:39:17 -0300

    fix: Teste notificação tarefa atualizada

--- src/test/java/br/com/unisinos/es/t2/application/domain/service/task/notification/discord/TaskUpdatedDiscordNotificationTest.java
+++ src/test/java/br/com/unisinos/es/t2/application/domain/service/task/notification/discord/TaskUpdatedDiscordNotificationTest.java
@@ -73,15 +73,16 @@ class TaskUpdatedDiscordNotificationTest {
         assertEquals(taskEvent.getTask().getTitle(), embed.getDescription());
 
         List<DiscordWebhookPayload.Field> fields = embed.getFields();
-        assertEquals(6, fields.size());
+        assertEquals(7, fields.size());
         // Quando há mudança de título, os campos "Título anterior" e "Título novo" devem ser exibidos
         assertField(fields.get(0), "Título anterior", "Old Title");
         assertField(fields.get(1), "Título novo", task.getTitle());
         assertField(fields.get(2), "Descrição", task.getDescription());
         assertField(fields.get(3), "Task ID", task.getId());
-        // Quando a tarefa é atualizada sem mudança de título, o campo "Título" deve ser zoomexibido
-        assertField(fields.get(4), "Vinculado a", taskEvent.getAssignee().getNameWithEmail());
-        assertField(fields.get(5), "Atualizada por", taskEvent.getTriggeredBy().getNameWithEmail());
+        // Quando a tarefa é atualizada sem mudança de título, o campo "Título" deve ser exibido
+        assertField(fields.get(4), "Status", task.getStatus().getDisplayName());
+        assertField(fields.get(5), "Vinculado a", taskEvent.getAssignee().getNameWithEmail());
+        assertField(fields.get(6), "Atualizada por", taskEvent.getTriggeredBy().getNameWithEmail());
     }
 
     @Test
@@ -121,15 +122,16 @@ class TaskUpdatedDiscordNotificationTest {
         assertEquals(taskEvent.getTask().getTitle(), embed.getDescription());
 
         List<DiscordWebhookPayload.Field> fields = embed.getFields();
-        assertEquals(6, fields.size());
+        assertEquals(7, fields.size());
         // Quando não há mudança de título, o campo "Título" deve ser exibido
         assertField(fields.get(0), "Título", task.getTitle());
         assertField(fields.get(1), "Descrição", task.getDescription());
         assertField(fields.get(2), "Task ID", task.getId());
         // Quando a tarefa é reatribuída, os campos "De" e "Para" devem ser exibidos
-        assertField(fields.get(3), "De", assigneeBefore.getNameWithEmail());
-        assertField(fields.get(4), "Para", assigneeAfter.getNameWithEmail());
-        assertField(fields.get(5), "Atualizada por", taskEvent.getTriggeredBy().getNameWithEmail());
+        assertField(fields.get(3), "Status", task.getStatus().getDisplayName());
+        assertField(fields.get(4), "De", assigneeBefore.getNameWithEmail());
+        assertField(fields.get(5), "Para", assigneeAfter.getNameWithEmail());
+        assertField(fields.get(6), "Atualizada por", taskEvent.getTriggeredBy().getNameWithEmail());
     }
 
     private void assertField(DiscordWebhookPayload.Field field, String expectedName, String expectedValue) {
